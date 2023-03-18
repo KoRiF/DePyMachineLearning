8942519ee04a8dc7a002a49aa1326ac32cc2436f
korif@tut.by <KoRiF>
2023-03-18 17:33:12 +0000

    python environment installation GUI

--- Unit1.pas
+++ Unit1.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Rtti,
   FMX.Grid.Style, FMX.StdCtrls, FMX.ScrollBox, FMX.Grid, FMX.TabControl,
-  FMX.Controls.Presentation, FMX.Memo.Types, FMX.Memo;
+  FMX.Controls.Presentation, FMX.Memo.Types, FMX.Memo, FMX.Edit;
 
 type
   TForm1 = class(TForm)
@@ -19,9 +19,16 @@ type
     TabItemEngineering: TTabItem;
     ButtonCreatePy: TButton;
     Memo1: TMemo;
+    AniIndicator1: TAniIndicator;
+    LabelPyStatus: TLabel;
+    ButtonFetchData: TButton;
+    EditMeteoStationId: TEdit;
+    LabelMeteostationId: TLabel;
     procedure ButtonCreatePyClick(Sender: TObject);
   private
     { Private declarations }
+    procedure SwitchProgressLabelsVisibility(displayed: Boolean);
+    procedure DisplayLabelsText(const status, details :string);
   public
     { Public declarations }
   end;
@@ -36,15 +43,32 @@ implementation
 uses UnitPyModule;
 
 procedure TForm1.ButtonCreatePyClick(Sender: TObject);
-var UpdateInstallStatus: TPyModule.TInstallStatusCallback;
+var UpdateInstallStatus: TPyModule.TStatusCallback;
 begin
   PyModule.PythonGUIInputOutput1.Output := Memo1; //Link from Python Module to GUI
 
   UpdateInstallStatus := procedure (const status, description: string; active: Boolean)
     begin
-
+      SwitchProgressLabelsVisibility(active);
+      DisplayLabelsText(status, description);
     end;
   PyModule.CreatePythonEnvironment(UpdateInstallStatus);
+end;
+
+
+procedure TForm1.DisplayLabelsText(const status, details: string);
+begin
+      LabelPyStatus.Text := status;
+      //LabelDetails.Text := details;
+      Memo1.Lines.Add(details);
+end;
+
+procedure TForm1.SwitchProgressLabelsVisibility(displayed: Boolean);
+begin
+    AniIndicator1.Enabled := displayed;
+    AniIndicator1.Visible := displayed;
+
+    LabelPyStatus.Visible := displayed;
 end;
 
 end.
