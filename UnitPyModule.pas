5f86dc4ec3e04be67e0ef5b61e94d086ce672a43
korif@tut.by <KoRiF>
2023-03-25 11:07:35 +0000

    implement K-Means clustering

--- UnitPyModule.pas
+++ UnitPyModule.pas
@@ -66,7 +66,9 @@ uses
   VarPyth, PyUtils;
 
 procedure TPyModule.ClusterizeMeteodata;
-const FIG0_PNG = 'MeteoData_Origin_plot.png';
+const
+  FIG0_PNG = 'MeteoData_Origin_plot.png';
+  FIG_KMEANS_PNG = 'MeteoData_K-Means-Clustered_plot.png';
 begin
   Pandas1.Import();
   var pd := Pandas1.pandas;
@@ -111,6 +113,91 @@ begin
   // Save the figure to a file
   var fig := plt.gcf();
   fig.savefig(FIG0_PNG);
+
+  // --------------------------------------------------
+  //Do clustering
+  ScikitLearn1.Import();
+  //var KMeans := ScikitLearn1.sklearn.KMeans;
+
+  var xyzcols := TPyEx.List(['temp', 'rhum', 'wmo']);
+  var df := _pymain.hdata.Values[xyzcols];
+  var xycols := TPyEx.List(['temp', 'rhum']);
+  var N_clusters := Length(meteoIds); //_pybin.len(wmos)
+
+
+  // Create a KMeans clustering model with N_clusters clusters
+  PythonEngine1.ExecString('from sklearn.cluster import KMeans');
+  var kmeans := ScikitLearn1.sklearn.cluster.KMeans(n_clusters:=N_clusters, n_init:=42, random_state:=13);
+  // Fit the model to the data frame
+  kmeans.fit(df.Values[xycols]);
+  // Get the cluster labels for each data point
+  var labels := kmeans.labels_;
+
+  // Create a new column in the data frame with the cluster labels
+  _pymain.df := df;
+  _pymain.labels := labels;
+  PythonEngine1.ExecString('df["cluster"] = labels'); //df.Values['cluster'] := labels;
+
+  var feature_label_majority_checker := function(df: Variant; const orig_label, cluster_label: String): Variant
+    begin
+      // Get the majority clustered_label for each original label group
+      var df_groupby_orig := df.groupby(orig_label);
+
+//      var lambda_counter := function (d: Variant): Variant
+//        begin
+//          RESULT := 1;//d.value_counts().index[0];
+//        end;
+      var lambda_counter := VarPythonEval('lambda d: d.value_counts().index[0]');
+
+      var label_cluster :=df_groupby_orig.Values[cluster_label];
+      var counted := label_cluster.agg(func:=lambda_counter);
+      var majority_labels := counted.to_dict();
+
+//      var lambda_checker := function (d: Variant): Variant
+//        begin
+//          var cluster_mark := d.loc.Values[cluster_label];
+//          var majority_mark := d.loc.Values[orig_label];
+//          RESULT := cluster_mark = majority_mark;
+//        end;
+      //_pyop.eq(df.Values[cluster_label], df.Values[orig_label])
+      _pymain.majority_labels := majority_labels;
+      _pymain.cluster_label := cluster_label;
+      _pymain.orig_label := orig_label;
+      var lambda_checker := VarPythonEval('lambda d: d[cluster_label]==majority_labels[d[orig_label]]');
+      RESULT := df.apply(lambda_checker, axis:=1);
+    end;
+
+  _pymain.kmeans_matched := feature_label_majority_checker(_pymain.df, 'wmo', 'cluster');
+
+  PythonEngine1.ExecString('df["kmeans_matched"] = kmeans_matched');
+
+  plt.scatter(_pymain.df.Values['temp'], _pymain.df.Values['rhum'], c:=_pymain.df.Values['wmo']);
+
+  // Get the points with incorrect labels (i.e. wmo_No<>labels)
+  PythonEngine1.ExecString('incorrect = df[~df["kmeans_matched"]]');
+  plt.scatter(_pymain.incorrect.Values['temp'], _pymain.incorrect.Values['rhum'], facecolors:='none', edgecolors:='r', alpha:=0.25, s:=100);
+
+  // Get the points with correct labels (i.e. wmo_No==labels)
+  PythonEngine1.ExecString('correct = df[df["kmeans_matched"]]');
+
+  _pybin.print(_pymain.majority_labels);
+  _pybin.print(_pymain.df);
+
+  plt.scatter(_pymain.correct.Values['temp'], _pymain.correct.Values['rhum'], facecolors:='none', edgecolors:='g', alpha:=0.125, s:=100);
+
+  // Add a color bar to the plot
+  plt.colorbar();
+
+  // Set the axis labels
+  plt.xlabel('Temperature (°C)');
+  plt.ylabel('Relative Humidity (%)');
+
+  // Show the plot
+  plt.show();
+
+  // Save the figure to a file
+  fig := plt.gcf();
+  fig.savefig(FIG_KMEANS_PNG);
 end;
 
 procedure TPyModule.CreatePythonEnvironment(InstallStatusCallback: TStatusCallback);
